2a3aa561c3471dd22e0d668aa024b87d0956f7cd
shashankBgowda <shashankbg74@gmail.com>
2025-08-10 10:24:10 -0500

    connected express with mongodb

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config(); 
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/mongodb.js';
+
+const app = express();
+
+const port = process.nextTick.PORT || 7070;
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+app.get("/", (req, res) =>{
+    return res.send("API is running");
+})
+
+app.listen(port, ()=>{
+    console.log(`server is up and running on port ${port}`);
+});
